49ac9405acdf67fda283e938192895057e9f2f26
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-06-06 20:10:14 +0700

    fix: add animation to back-to-top button

--- assets/js/scripts/back-to-top.ts
+++ assets/js/scripts/back-to-top.ts
@@ -13,10 +13,28 @@ window.onscroll = function () {
 function scrollFunction() {
   const thebutton = document.getElementById("back-to-top");
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    thebutton?.classList.add('d-block');
-    thebutton?.classList.remove('d-none');
+    thebutton?.classList.remove('d-none', 'animate__animated', 'animate__fadeOut');
+    thebutton?.classList.add('d-block', 'animate__animated', 'animate__fadeIn');
   } else {
-    thebutton?.classList.add('d-none');
-    thebutton?.classList.remove('d-block');
+    thebutton?.classList.remove('d-block', 'animate__animated', 'animate__fadeIn');
+    thebutton?.classList.add('d-none', 'animate__animated', 'animate__fadeOut');
   }
 }
+
+// const animateCSS = (element, animation, prefix = 'animate__') =>
+//   // We create a Promise and return it
+//   new Promise((resolve, reject) => {
+//     const animationName = `${prefix}${animation}`;
+//     const node = document.querySelector(element);
+
+//     node.classList.add(`${prefix}animated`, animationName);
+
+//     // When the animation ends, we clean the classes and resolve the Promise
+//     function handleAnimationEnd(event) {
+//       event.stopPropagation();
+//       node.classList.remove(`${prefix}animated`, animationName);
+//       resolve('Animation ended');
+//     }
+
+//     node.addEventListener('animationend', handleAnimationEnd, { once: true });
+//   });
