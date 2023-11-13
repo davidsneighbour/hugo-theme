374e15968db424e4af49a15ff237ca949f4677d8
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-11-13 19:46:40 +0700

    feat: add collapsible shortcode

--- theme/assets/js/scripts/bs-collapsibles.ts
+++ theme/assets/js/scripts/bs-collapsibles.ts
@@ -3,5 +3,5 @@ import { Collapse } from 'bootstrap';
 // Bootstrap Collapsibles
 const collapseElementList = Array.prototype.slice.call(document.querySelectorAll('.collapse'));
 collapseElementList.forEach((collapseElement: Element) => new Collapse(collapseElement, {
-	toggle: false,
+  toggle: false,
 }).hide());
