93a92ba78e72ecea394a314efad99f781bc86313
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-02-12 15:03:33 +0700

    fix: remove clickspark
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- theme/assets/js/script.js
+++ theme/assets/js/script.js
@@ -1,10 +1,10 @@
 // import 'web-vitals-element';
 import 'web-vitals-element';
 import './scripts/keyboard-layout';
-import ClickSpark from './scripts/click-effect';
+// import ClickSpark from './scripts/click-effect';
 
 // import bootstrap scripts
 import './scripts/bs-tabs.js';
 import './scripts/bs-tooltips.js';
 
-customElements.define("click-effect", ClickSpark);
+// customElements.define("click-effect", ClickSpark);
