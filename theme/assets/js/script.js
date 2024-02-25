00a6e92e316617d0077299abb6fa9d7d4e5bf04c
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-02-25 19:58:38 +0700

    theme: some acrobatics with headlines on single post pages
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- theme/assets/js/script.js
+++ theme/assets/js/script.js
@@ -1,6 +1,8 @@
 import 'web-vitals-element';
 import './scripts/keyboard-layout';
 
+import './scripts/theme-changes';
+
 // import bootstrap scripts
 import './scripts/bs-tabs.js';
 import './scripts/bs-tooltips.js';
